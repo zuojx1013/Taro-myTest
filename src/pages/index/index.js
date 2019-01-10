394146a0af78c3d6ed6f889a71ab9738bcf383c4
zuojx <zuojx1013@163.com>
2019-01-10 08:58:38 +0800

    taro报错问题

---
+++ src/pages/index/index.js
@@ -0,0 +1,31 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View, Text } from '@tarojs/components'
+import './index.scss'
+import { AtButton } from 'taro-ui'
+
+export default class Index extends Component {
+
+  config = {
+    navigationBarTitleText: '首页'
+  }
+
+  componentWillMount () { }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='index'>
+        <Text>Hello world!</Text>
+        <AtButton>按钮文案</AtButton>
+      </View>
+    )
+  }
+}
+
